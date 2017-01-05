2887fa9f21dd66cacf3cd0ab6ec2ede9022b1e15
Your Name <you@example.com>
2017-01-05 19:44:47 +0900

    Allow this plugin to copy approved page to specified other dokuwiki.

--- README.txt
+++ README.txt
@@ -1,1 +1,28 @@
 Plug-in to allow simple page approval process.
+
+2017-1-5 coauthoring
+Purpose: Introducing hierarchical approval process and/or
+         more secure internal draft protection.
+Feature: Allowing this plugin copying an approved page to
+         a specified other dokuwiki (e.g. public site to internet)
+Modify:  Modified in action/approve.php
+Usage:   set configuration below in conf/local.php (Please manually
+         add those code.)
+-----
+$conf['debuglogfile'] = '/path/to/dokuwiki/log/debuglog.txt';
+$conf['copybeforeapprove'] = '/path/to/dokuwiki/data'; //data directory path without "/"
+$conf['isdebugging'] = true; //set false if you do not need the log.
+$conf['iscopyremote'] = false; //set true if you use ftp for remote site
+$conf['ftp_server'] = 'dokuwiki.my-remote-site.com';
+$conf['ftp_user_name'] = 'ftp-user';
+$conf['ftp_user_pass'] = 'ftp-user-password';
+$conf['remoteftport'] = 21; //set ftp port
+$conf['remotefttimeout'] = 90; //set timeout (second)
+-----
+         When you approve a page in your primary (drafting) dokuwiki,
+         it will be copied to the secondary (publishing) dokuwiki as a
+         non-approved page. If the page be approved again on the
+         secondary site, the page is publised on the site.
+Future work:
+         Plugin configuration forms.
+
--- action/approve.php
+++ action/approve.php
@@ -94,6 +94,103 @@ class action_plugin_publish_approve extends DokuWiki_Action_Plugin {
             time()
         );
 
+        /* contents copy hack */
+        if($conf['isdebugging']){
+          file_put_contents($conf['debuglogfile'],"------\n",FILE_APPEND);//debug
+        }
+        global $conf;
+        $iscopysuccess=false;
+        $iscopysuccess2=false;
+        $iscopysuccess3=false;
+        $iscopysuccess4=false;
+        $pagelog = new PageChangeLog(getID());
+        $cpfrev = $pagelog->getRevisions(0, 1);
+
+        if($conf['isdebugging']){
+          ob_start();
+          var_dump($pagelog);
+          var_dump($cpfrev);
+          $buffer = ob_get_contents();
+          ob_end_clean();
+        }
+
+        $frommetafn = metaFN($ID,'.meta');
+        $pos = strrpos($frommetafn, '/');
+        if ($pos!==false) $frommetadir = substr($frommetafn, 0, $pos+1);
+        $frompagefn = wikiFN($ID);
+        $pagetitle = noNS($ID);
+        $namespace = getNS($ID);
+        $distmetadir=$conf['copybeforeapprove']."/data/meta/".(strlen($namespace)>0?$namespace."/":"");
+        $distatticdir=$conf['copybeforeapprove']."/data/attic/".(strlen($namespace)>0?$namespace."/":"");
+        $distmetafn=$pagetitle.".meta";
+        $changesfn=$pagetitle.".changes";
+        $atticfn=$pagetitle.".".$cpfrev[0].".txt.gz";
+        $fromatticfn=wikiFN($ID,$cpfrev[0]);
+        $distpagedir=$conf['copybeforeapprove']."/data/pages/".(strlen($namespace)>0?$namespace."/":"");
+        $distpagefn=$pagetitle.".txt";
+
+        $conn_id = null;
+        if($conf['iscopyremote']){ //try copying to remote server via ftp
+          $conn_id = ftp_login($conf['ftp_server'], $conf['ftp_user_name'], $conf['ftp_user_pass']);
+          if (!$conn_id && $conf['isdebugging']) { //debug
+            file_put_contents($conf['debuglogfile'],"------\n",FILE_APPEND);
+            file_put_contents($conf['debuglogfile'],"can not login\n",FILE_APPEND);
+            file_put_contents($conf['debuglogfile'],$conf['ftp_server']."\n",FILE_APPEND);
+            file_put_contents($conf['debuglogfile'],$conf['ftp_user_name']."\n",FILE_APPEND);
+            file_put_contents($conf['debuglogfile'],$conf['ftp_user_pass']."\n",FILE_APPEND);
+            ftp_close($conn_id);
+            die("can't login");
+          }
+          if(!ftp_chdir($conn_id, $distmetadir))ftp_mkdir($conn_id,$distmetadir);
+          if(!ftp_chdir($conn_id, $distatticadir))ftp_mkdir($conn_id,$distatticadir);
+          if(!ftp_chdir($conn_id, $distpagedir))ftp_mkdir($conn_id,$distpagedir);
+        }else{ // try local copy
+          if(!file_exists($distmetadir))mkdir($distmetadir,0755,true);
+          if(!file_exists($distatticadir))mkdir($distatticadir,0755,true);
+          if(!file_exists($distpagedir))mkdir($distpagedir,0755,true);
+        }
+
+        if($conf['isdebugging']){ //debug
+          file_put_contents($conf['debuglogfile'],"------\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],$ID ."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],$fromatticfn ."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],$distatticdir ."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],$atticfn ."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],"frompagefn ".$frompagefn ."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],"frommetadir ".$frommetadir ."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],"frommetafn ".$frommetafn ."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],"frommetachanges".$frommetadir.$changesfn."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],"namespace ".$namespace ."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],"pagetitle ".$pagetitle ."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],"distinations \n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],$distmetadir.$distmetafn."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],$distmetadir.$changesfn."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],$distpagedir.$distpagefn."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],$distatticdir.$atticfn."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],"------\n",FILE_APPEND);//debug
+        }
+
+        if($conf['iscopyremote']){
+          $iscopysuccess = ftp_put($conn_id,$distpagedir.$distpagefn,$frompagefn);
+          $iscopysuccess2 = ftp_put($conn_id,$distmetadir.$distmetafn,$frommetafn);
+          $iscopysuccess3 = ftp_put($conn_id,$distmetadir.$changesfn,$frommetadir.$changesfn);
+          $iscopysuccess4 = ftp_put($conn_id,$distatticdir.$atticfn,$fromatticfn);
+        }else{
+          $iscopysuccess = copy($frompagefn,$distpagedir.$distpagefn);
+          $iscopysuccess2 = copy($frommetafn,$distmetadir.$distmetafn);
+          $iscopysuccess3 = copy($frommetadir.$changesfn,$distmetadir.$changesfn);
+          $iscopysuccess4 = copy($fromatticfn,$distatticdir.$atticfn);
+        }
+
+        if($conf['isdebugging']){ //debug
+          if($iscopysuccess && $iscopysuccess2 && $iscopysuccess3 && $iscopysuccess4)
+            file_put_contents($conf['debuglogfile'],$ID." copy success"."\n",FILE_APPEND);
+          else
+            file_put_contents($conf['debuglogfile'],$ID." copy failed"."\n",FILE_APPEND);
+          file_put_contents($conf['debuglogfile'],$ID." passed"."\n",FILE_APPEND);
+        }
+        /* contents copy hack end */
+
         $success = p_set_metadata($ID, array('approval' => $approvals), true, true);
         if ($success) {
             msg($this->getLang('version approved'), 1);
